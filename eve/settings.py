e85e5324211038bfed6edf52c51826636c6a574c
Владимир Тюрин <vlad89main@gmail.com>
2024-01-29 21:09:10 +0300

    Minor changes to settings

--- eve/settings.py
+++ eve/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-from .config import MYSQL_PASSWORD
+from .config import MYSQL_PASSWORD, SECRET_KEY
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bo50p+68gd&5#ksfpe@)5)w*-ho$n8(c065zc3kg0i(^sp&+w='
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
