5f6cdb66cd779561362a430c0f26aefab4984366
Владимир Тюрин <vlad89main@gmail.com>
2024-01-31 12:22:51 +0300

    Edited docker-compose

--- eve/settings.py
+++ eve/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['em58922.tw1.ru']
 
 
 # Application definition
